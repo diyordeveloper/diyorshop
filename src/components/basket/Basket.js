5986995fb4a636c8279bbf8a546a0fc775b0a38b
Diyorbek Developer <xidirovdiyorbek6@gmail.com>
2022-07-23 10:35:27 +0500

    form

--- src/components/basket/Basket.js
+++ src/components/basket/Basket.js
@@ -7,12 +7,38 @@ import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import NavFot from "../navfot/NavFot";
 import "./basket.scss";
 import BasketCard from "./BasketCard";
-function Basket({args}) {
-  const { basket, totalQty, totalPrice } = useUserContext();
+import { toast } from "react-toastify";
+function Basket({}) {
+  const { basket, totalQty, totalPrice, user } = useUserContext();
   const [modalToggle, setModalToggle] = useState(false);
-  function Toggle(){
-    setModalToggle(!modalToggle)
+  function Toggle() {
+    setModalToggle(!modalToggle);
     console.log("open modal");
+  }
+  function onSubmitForm(e){
+    e.preventDefault()
+    const name = e.target[0].value
+    const phone = e.target[1].value
+    const region = e.target[2].value
+    const district = e.target[3].value
+    const target = e.target[4].value
+    const house = e.target[5].value
+    const explanation = e.target[6].value
+    if(
+      name !== '',
+      phone !== '',
+      region !== '',
+      district !== '',
+      target !== '',
+      house !== '',
+      explanation !== ''){
+        console.log('successfuly bratan');
+        toast.success("buyurtmangiz tez orada yetkazib beriladi")
+        Toggle()
+      }
+      else{
+        toast.error("Fill in the field")
+      }
   }
   return (
     <NavFot>
@@ -36,7 +62,7 @@ function Basket({args}) {
                 <h2 className="text-center text-primary ">Cart Summary</h2>
                 <h4>Total No of Products: {totalQty}</h4>
                 <h4>Total Price to Pay: $ - {totalPrice} </h4>
-                <button className="btn btn-primary mt-3 "onClick={Toggle}>
+                <button className="btn btn-primary mt-3 " onClick={Toggle}>
                   Cash on Delivery
                 </button>
               </div>
@@ -54,13 +80,40 @@ function Basket({args}) {
           </h2>
         )}
       </div>
-      <Modal isOpen={modalToggle} toggle={Toggle} {...args}>
+      <Modal isOpen={modalToggle} toggle={Toggle}>
         <ModalHeader>
-          <h5>Quantity: {totalQty}</h5>
-          <h5>Price: {totalPrice}</h5>
+          <h5 className="text-center">
+            Quantity: <span className="text-primary">{totalQty}</span>
+          </h5>
+          <h5 className="text-center">
+            Price: $ - <span className="text-primary">{totalPrice} </span>
+          </h5>
         </ModalHeader>
-        <ModalBody></ModalBody>
-        <ModalFooter></ModalFooter>
+        <ModalBody>
+          <form id="shop" onSubmit={onSubmitForm}>
+            <input
+              type="text"
+              placeholder="First Name and Last Name"
+              defaultValue={user?.name}
+              className="form-control"
+            />
+            <input
+              type="number"
+              placeholder="Phone"
+              defaultValue={user?.phone}
+              className="form-control mt-2"
+            />
+            <input type="text" placeholder="Region" className="form-control mt-2" />
+            <input type="text" placeholder="District" className="form-control mt-2" />
+            <input type="text" placeholder="Target" className="form-control mt-2" />
+            <input type="text" placeholder="Street name and house number" className="form-control mt-2" />
+            <textarea className="form-control mt-2" placeholder="Explanation" ></textarea>
+          </form>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-danger w-100" onClick={Toggle}>Cancel</button>
+          <button form="shop" className="btn btn-success w-100">Placing an order $ - {totalPrice}</button>
+        </ModalFooter>
       </Modal>
     </NavFot>
   );
